ce0c7ea966af5408380135418c074ef692417ff2
Brendan Weibrecht <brendan@weibrecht.net.au>
2021-03-19 04:18:11 +1100

    Detect fullscreen transition finish rather than using a fixed timeout
    Contributes to #2. I've not yet tested it in Vivaldi, but hopefully that's resolved now.

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube fast fullscreen toggle
 // @namespace    http://tampermonkey.net/
-// @version      2.2.0
+// @version      2.3.0
 // @description  Avoids the ~3 second lag when entering/exiting fullscreen on a YouTube video - by hiding the heavy fluff while transitioning
 // @author       Brendan Weibrecht
 // @match        https://www.youtube.com/watch*
@@ -21,11 +21,43 @@
     const hideFluff = () => setFluffDisplay('none')
     const showFluff = () => setFluffDisplay('')
 
+    const checkIfDirectionIsLeavingFullscreen = () => document.querySelector('ytd-watch-flexy').fullscreen
+
+    const isFinishedLeavingFullscreen = () => {
+        const video = document.querySelector('ytd-player')
+        return Math.abs(video.clientWidth - window.innerWidth) > 1 &&
+               video.clientHeight != window.innerHeight
+    }
+    const isFinishedEnteringFullscreen = () => {
+        const video = document.querySelector('ytd-player')
+        return Math.abs(video.clientWidth - window.innerWidth) <= 1 &&
+               video.clientHeight == window.innerHeight
+    }
+
+    const transitionIsFinished = (directionIsLeavingFullscreen) => (
+        directionIsLeavingFullscreen && isFinishedLeavingFullscreen() ||
+        !directionIsLeavingFullscreen && isFinishedEnteringFullscreen()
+    )
+
+    const timedOut = (startedAt) => new Date() - startedAt >= 2000
+
+    const showFluffWhenReady = (directionIsLeavingFullscreen, startedAt) => {
+        if (timedOut(startedAt)) {
+            console.log(`[YT-FFT] Timed out detecting finish of ${directionIsLeavingFullscreen ? 'leaving' : 'entering'} fullscreen transition; showing fluff again`)
+            showFluff()
+        } else if (transitionIsFinished(directionIsLeavingFullscreen)) {
+            console.log(`[YT-FFT] Finished ${directionIsLeavingFullscreen ? 'leaving' : 'entering'} fullscreen transition; showing fluff again`)
+            showFluff()
+        } else {
+            setTimeout(() => showFluffWhenReady(directionIsLeavingFullscreen, startedAt), 10)
+        }
+    }
+
     const fastToggleFullScreen = () => {
         hideFluff()
-        setTimeout(() => {
-            setTimeout(showFluff, 10)
-        }, 10)
+        const directionIsLeavingFullscreen = checkIfDirectionIsLeavingFullscreen()
+        console.log(`[YT-FFT] ${directionIsLeavingFullscreen ? 'Leaving' : 'Entering'} fullscreen; fluff has been hidden`)
+        showFluffWhenReady(directionIsLeavingFullscreen, new Date())
     }
 
     const isWritingText = (e) => (
@@ -34,6 +66,10 @@
     )
 
     document.querySelector('.ytp-fullscreen-button').addEventListener("click", e => {
+        fastToggleFullScreen()
+    })
+
+    document.querySelector('#player').addEventListener('dblclick', e => {
         fastToggleFullScreen()
     })
 
