c3798877d6f33b8ead4c39e167de36b6a38ada80
Brendan Weibrecht <brendan@weibrecht.net.au>
2021-03-20 21:58:24 +1100

    Delay a little after entering fullscreen for reliability, since delay before showing the fluff when entering fullscreen is not noticeable
    I couldn't do this before, since I wasn't separating the paths for entering/leaving

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -18,8 +18,14 @@
         fluff.forEach(sel => { document.querySelector(sel).style.display = value })
     }
 
-    const hideFluff = () => setFluffDisplay('none')
-    const showFluff = () => setFluffDisplay('')
+    const hideFluff = () => {
+        console.log(`[YT-FFT] Hiding fluff`)
+        setFluffDisplay('none')
+    }
+    const showFluff = () => {
+        console.log(`[YT-FFT] Showing fluff again`)
+        setFluffDisplay('')
+    }
 
     const isInFullscreen = () => !!document.fullscreenElement
 
@@ -46,11 +52,12 @@
 
     const showFluffWhenReady = (directionIsLeavingFullscreen, startedAt) => {
         if (timedOut(startedAt)) {
-            console.log(`[YT-FFT] Timed out detecting finish of ${directionIsLeavingFullscreen ? 'leaving' : 'entering'} fullscreen transition; showing fluff again`)
+            console.log(`[YT-FFT] Timed out detecting finish of ${directionIsLeavingFullscreen ? 'leaving' : 'entering'} fullscreen transition`)
             showFluff()
         } else if (transitionIsFinished(directionIsLeavingFullscreen)) {
-            console.log(`[YT-FFT] Finished ${directionIsLeavingFullscreen ? 'leaving' : 'entering'} fullscreen transition; showing fluff again`)
-            showFluff()
+            console.log(`[YT-FFT] Finished ${directionIsLeavingFullscreen ? 'leaving' : 'entering'} fullscreen transition`)
+            // Delay a little after entering fullscreen for reliability, since delay before showing the fluff when entering fullscreen is not noticeable
+            directionIsLeavingFullscreen ? showFluff() : setTimeout(showFluff, 20)
         } else {
             setTimeout(() => showFluffWhenReady(directionIsLeavingFullscreen, startedAt), 10)
         }
@@ -58,7 +65,6 @@
 
     const fastToggleFullScreen = () => {
         hideFluff()
-        console.log('[YT-FFT] Fluff hidden')
         // Wait until YouTube's event handler has executed. This is required since the order of event handler execution is not guaranteed, and we need to be able to reliably detect the transition direction
         setTimeout(() => {
             const directionIsLeavingFullscreen = checkIfDirectionIsLeavingFullscreen()
